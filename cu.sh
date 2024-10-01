df31ac861d4e2202a7be9016c39710d4bc569ace
bpk-ctrl <benpk19@duck.com>
2024-10-01 20:02:38 +0200

    Add files via upload

---
+++ cu.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Letöltés..."
+wget -b https://raw.githubusercontent.com/bpk-ctrl/sh/refs/heads/main/kirugás.sh
+sudo chmod +x kirugás.sh
+sudo cp kirugás.sh /usr/bin/kirugas
+sleep 1
+echo "Letöltve."
+kirugas
